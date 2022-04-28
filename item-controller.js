695a1e992f4254b64d73e01f51021f036b049bfc
Diegonaza <diegonaza0@gmail.com>
2022-04-28 20:08:47 +0000

    Routing and Controller

---
+++ item-controller.js
@@ -0,0 +1,9 @@
+exports.helloWorld = function(req,res){
+   
+        res.json({message: "Hello World",
+                            "data":[
+                                req.params.foo,
+                                req.params.bar
+                            ]  })
+   
+}
