837026b523a1c7ac57438d7db18f3954c1fe3324
Diegonaza <diegonaza0@gmail.com>
2022-05-12 19:23:14 +0000

    ImageUpload

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@ const express = require("express"),
       router = express.Router(),
       itemCtrl = require("./item-controller"),
       userCtrl = require("./user-controller");
+      var multer = require('multer'), 
+      upload = multer({ dest: module.exports.UPLOAD_PATH }), 
+      imageCtrl = require('./image-controller');
 
 //router.get("/:foo/:bar", itemCtrl.helloWorld);
 router.post('/users', userCtrl.createUser);
@@ -10,4 +13,10 @@ router.get('/users/:_username', userCtrl.getUser);
 router.put('/users/:_username', userCtrl.updateUser);
 router.delete('/users/:id', userCtrl.deleteUser);
 
+router.post('/images', upload.single('image'), imageCtrl.uploadImage); 
+router.get('/images', imageCtrl.getImages); 
+router.get('/images/:id', imageCtrl.getImage); 
+router.delete('/images/:id', imageCtrl.deleteImage);
+
 module.exports = router;
+module.exports.UPLOAD_PATH = 'uploads'; 
