32de3e44e005b16b0602bff44f5c2362812bac7d
Diegonaza <diegonaza0@gmail.com>
2022-05-12 17:45:21 +0000

    Added single user search

--- user-controller.js
+++ user-controller.js
@@ -19,3 +19,13 @@ exports.getUsers = function(req, res) {
       res.json(users); 
     });  
   };
+
+  exports.getUser = function(req, res) { 
+    User.findOne({_id: req.params.id}, function (err, user) { 
+      if (err) { 
+        res.status(400).json(err); 
+      }  
+      res.json(user); 
+    });  
+  };
+
